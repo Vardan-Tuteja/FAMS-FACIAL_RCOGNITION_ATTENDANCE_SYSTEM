89f4bdd02de0fb1ac28fc4dc69b28c8570d0712f
Vardan-Tuteja <159055508+Vardan-Tuteja@users.noreply.github.com>
2025-02-04 15:46:11 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from cx_Freeze import setup, Executable
+
+setup(
+
+       name="AUTOMATIC FACE DETECTION ATTENDANCE SYSTEM",
+
+       version="1.0",
+
+       description="",
+
+       executables=[Executable("AMS_Run.py")],
+
+   )   
